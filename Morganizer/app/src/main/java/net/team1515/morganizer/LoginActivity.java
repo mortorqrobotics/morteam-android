bc059f4546afa347a3e334eaf856db4d99847919
DaPasta <484902@gmail.com>
2015-06-11 20:13:52 -0700

    Adds navigation activity and fragment

--- Morganizer/app/src/main/java/net/team1515/morganizer/LoginActivity.java
+++ Morganizer/app/src/main/java/net/team1515/morganizer/LoginActivity.java
@@ -1,11 +1,9 @@
 package net.team1515.morganizer;
 
-import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.support.v7.app.ActionBarActivity;
 import android.view.Menu;
 import android.view.MenuItem;
-
-import morganizer.mortorq.com.morganizer.R;
 
 
 public class LoginActivity extends ActionBarActivity {
