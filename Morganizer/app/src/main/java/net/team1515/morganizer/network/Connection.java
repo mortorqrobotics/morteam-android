bddc72a884e4dccc9b5b054a3305c1801080717c
DaPasta <484902@gmail.com>
2015-06-23 23:01:35 -0700

    Experimental http code

--- Morganizer/app/src/main/java/net/team1515/morganizer/network/Connection.java
+++ Morganizer/app/src/main/java/net/team1515/morganizer/network/Connection.java
@@ -1,11 +1,18 @@
 package net.team1515.morganizer.network;
 
+import android.util.Log;
+
+import org.apache.http.HttpResponse;
 import org.apache.http.NameValuePair;
+import org.apache.http.client.ClientProtocolException;
 import org.apache.http.client.HttpClient;
+import org.apache.http.client.entity.UrlEncodedFormEntity;
 import org.apache.http.client.methods.HttpPost;
 import org.apache.http.impl.client.DefaultHttpClient;
 import org.apache.http.message.BasicNameValuePair;
 
+import java.io.IOException;
+import java.io.UnsupportedEncodingException;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -13,9 +20,37 @@ import java.util.List;
  * Created by david on 6/11/15.
  */
 public class Connection {
-    HttpClient client = new DefaultHttpClient();
-    HttpPost post = new HttpPost("127.0.0.1:8080/loginUser");
+    HttpClient httpClient = new DefaultHttpClient();
+    // replace with your url
+    HttpPost httpPost = new HttpPost("www.example.com");
 
-    List<BasicNameValuePair> pairs = new ArrayList<>();
+
+    //Post Data
+    List<NameValuePair> nameValuePair = new ArrayList<NameValuePair>(2);
+    nameValuePair.add(new BasicNameValuePair("username", "test_user"));
+    nameValuePair.add(new BasicNameValuePair("password", "123456789"));
+
+
+
+    //Encoding POST data
+    try {
+        httpPost.setEntity(new UrlEncodedFormEntity(nameValuePair));
+    } catch (UnsupportedEncodingException e) {
+        // log exception
+        e.printStackTrace();
+    }
+
+    //making POST request.
+    try {
+        HttpResponse response = httpClient.execute(httpPost);
+        // write response to log
+        Log.d("Http Post Response:", response.toString());
+    } catch (ClientProtocolException e) {
+        // Log exception
+        e.printStackTrace();
+    } catch (IOException e) {
+        // Log exception
+        e.printStackTrace();
+    }
 
 }
