2c0cfd6fec1aa61eb13c0a8c8a0d374a06dad882
DaPasta <484902@gmail.com>
2015-11-21 17:17:11 -0800

    Adds ability to choose audience for announcements (communication with server not working 100%)

--- Morganizer/app/src/main/java/net/team1515/morteam/fragment/HomeFragment.java
+++ Morganizer/app/src/main/java/net/team1515/morteam/fragment/HomeFragment.java
@@ -15,8 +15,11 @@ import android.text.format.DateFormat;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.ImageButton;
 import android.widget.ImageView;
+import android.widget.Spinner;
 import android.widget.TextView;
 
 import com.android.volley.Request;
@@ -38,15 +41,24 @@ import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Locale;
 import java.util.Map;
 
 public class HomeFragment extends Fragment {
 
+    private RequestQueue queue;
     private SharedPreferences preferences;
-    private AnnouncementAdapter adapter;
-    private SwipeRefreshLayout swipeLayout;
+    private AnnouncementAdapter announcementAdapter;
+    private SwipeRefreshLayout refreshLayout;
     private SlidingUpPanelLayout slidingLayout;
+
+    private Spinner choiceSpinner;
+    private final Map<String, String> choices = new HashMap<>();
+    private String currentPostGroup;
+
+    private final Map<String, String> users = new HashMap<>();
+    private final Map<String, String> subdivisions = new HashMap<>();
 
 
     @Override
@@ -54,19 +66,27 @@ public class HomeFragment extends Fragment {
         View view = inflater.inflate(R.layout.fragment_home, container, false);
 
         preferences = getActivity().getSharedPreferences(null, 0);
+        queue = Volley.newRequestQueue(getContext());
+        queue.addRequestFinishedListener(new RequestQueue.RequestFinishedListener<Object>() {
+            @Override
+            public void onRequestFinished(Request<Object> request) {
+                announcementAdapter.notifyDataSetChanged();
+                populateChoiceSpinner();
+            }
+        });
 
         RecyclerView recyclerView = (RecyclerView) view.findViewById(R.id.announcement_view);
         RecyclerView.LayoutManager layoutManager = new LinearLayoutManager(getActivity());
         recyclerView.setLayoutManager(layoutManager);
-        adapter = new AnnouncementAdapter();
-        recyclerView.setAdapter(adapter);
+        announcementAdapter = new AnnouncementAdapter();
+        recyclerView.setAdapter(announcementAdapter);
 
-        swipeLayout = (SwipeRefreshLayout) view.findViewById(R.id.swipe_layout);
-        swipeLayout.setColorSchemeResources(R.color.orange_theme);
-        swipeLayout.setOnRefreshListener(new SwipeRefreshLayout.OnRefreshListener() {
+        refreshLayout = (SwipeRefreshLayout) view.findViewById(R.id.swipe_layout);
+        refreshLayout.setColorSchemeResources(R.color.orange_theme);
+        refreshLayout.setOnRefreshListener(new SwipeRefreshLayout.OnRefreshListener() {
             @Override
             public void onRefresh() {
-                adapter.requestAnnouncements();
+                announcementAdapter.requestAnnouncements();
             }
         });
 
@@ -74,7 +94,148 @@ public class HomeFragment extends Fragment {
         slidingLayout.setPanelState(SlidingUpPanelLayout.PanelState.HIDDEN);
         slidingLayout.setTouchEnabled(false);
 
+        //Populate choice spinner
+        choiceSpinner = (Spinner) slidingLayout.findViewById(R.id.new_choicespinner);
+        choiceSpinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                String item = parent.getItemAtPosition(position).toString();
+                if (item.equals("Everyone")) {
+                    currentPostGroup = "everyone";
+                } else if (!item.equals("Custom")) {
+                    currentPostGroup = "\"subdivisionMembers\":[\"" + choices.get(item) + "\"],\"userMembers\":[]";
+                } else {
+                    AlertDialog.Builder builder = new AlertDialog.Builder(HomeFragment.this.getContext());
+                    builder.setTitle("Choose an audience");
+                    final List<String> subdivisionIds = new ArrayList<String>();
+                    final List<String> userIds = new ArrayList<String>();
+                    final List<CharSequence> audiences = new ArrayList<CharSequence>();
+                    for (String subdivision : subdivisions.keySet()) {
+                        audiences.add(subdivision);
+                    }
+                    for (String user : users.keySet()) {
+                        audiences.add(user);
+                    }
+                    builder.setMultiChoiceItems(audiences.toArray(new CharSequence[audiences.size()]), null, new DialogInterface.OnMultiChoiceClickListener() {
+                        @Override
+                        public void onClick(DialogInterface dialog, int which, boolean isChecked) {
+                            if (isChecked) {
+                                String key = audiences.get(which).toString();
+                                if (subdivisions.containsKey(key)) {
+                                    subdivisionIds.add(subdivisions.get(key));
+                                } else {
+                                    userIds.add(users.get(key));
+                                }
+                            }
+                        }
+                    });
+                    builder.setPositiveButton("Choose", new DialogInterface.OnClickListener() {
+                        @Override
+                        public void onClick(DialogInterface dialog, int which) {
+                            currentPostGroup = "{\"subdivisionMembers\":[";
+                            for(String id : subdivisionIds) {
+                                currentPostGroup += "\"" + id + "\",";
+                            }
+                            currentPostGroup = currentPostGroup.substring(0, currentPostGroup.length() - 1);
+                            currentPostGroup += "],\"userMemebers\":[";
+                            for(String id : userIds) {
+                                currentPostGroup += "\"" + id  + "\",";
+                            }
+                            currentPostGroup = currentPostGroup.substring(0, currentPostGroup.length() - 1);
+                            currentPostGroup += "]}";
+                        }
+                    });
+                    builder.setNegativeButton("Cancel", null);
+                    builder.create().show();
+                }
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+
+            }
+        });
+
+        //Get users and subdivisions
+        CookieRequest usersRequest = new CookieRequest(
+                Request.Method.POST,
+                "/f/getUsersInTeam",
+                preferences,
+                new Response.Listener<String>() {
+                    @Override
+                    public void onResponse(String response) {
+                        try {
+                            JSONArray userArray = new JSONArray(response);
+                            for (int i = 0; i < userArray.length(); i++) {
+                                JSONObject userObject = userArray.getJSONObject(i);
+                                users.put(
+                                        userObject.getString("lastname") + " " + userObject.getString("firstname"),
+                                        userObject.getString("_id")
+                                );
+                            }
+                        } catch (JSONException e) {
+                            e.printStackTrace();
+                        }
+                    }
+                },
+                new Response.ErrorListener() {
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+                        System.out.println(error);
+                    }
+                }
+        );
+
+        CookieRequest subdivisionsRequest = new CookieRequest(Request.Method.POST,
+                "/f/getAllSubdivisionsForUserInTeam",
+                preferences,
+                new Response.Listener<String>() {
+                    @Override
+                    public void onResponse(String response) {
+                        try {
+                            JSONArray subdivisionArray = new JSONArray(response);
+                            for (int i = 0; i < subdivisionArray.length(); i++) {
+                                JSONObject subdivisionObject = subdivisionArray.getJSONObject(i);
+                                subdivisions.put(
+                                        subdivisionObject.getString("name"),
+                                        subdivisionObject.getString("_id")
+                                );
+                            }
+                        } catch (JSONException e) {
+                            e.printStackTrace();
+                        } finally {
+                            //Populate spinner choice map
+                            choices.clear();
+                            choices.put("Everyone", "Everyone");
+                            choices.putAll(subdivisions);
+                            choices.put("Custom", "Custom");
+                        }
+                    }
+                },
+                new Response.ErrorListener() {
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+                        System.out.println(error);
+                    }
+                }
+        );
+        queue.add(subdivisionsRequest);
+        queue.add(usersRequest);
+
         return view;
+    }
+
+    private void populateChoiceSpinner() {
+        choiceSpinner.setAdapter(
+                new ArrayAdapter<>(
+                        getContext(),
+                        R.layout.support_simple_spinner_dropdown_item,
+                        new ArrayList<>(choices.keySet()))
+        );
+    }
+
+    public String getCurrentPostGroup() {
+        return currentPostGroup;
     }
 
     public void openNewAnnouncement() {
@@ -93,26 +254,17 @@ public class HomeFragment extends Fragment {
     }
 
     public void requestAnnouncements() {
-        adapter.requestAnnouncements();
+        announcementAdapter.requestAnnouncements();
     }
 
     public class AnnouncementAdapter extends RecyclerView.Adapter<AnnouncementAdapter.ViewHolder> {
 
         private CookieRequest announcementsRequest;
-        private RequestQueue queue;
 
         private ArrayList<Announcement> announcements;
 
         public AnnouncementAdapter() {
             announcements = new ArrayList<>();
-            queue = Volley.newRequestQueue(getContext());
-            queue.addRequestFinishedListener(new RequestQueue.RequestFinishedListener<Object>() {
-                @Override
-                public void onRequestFinished(Request<Object> request) {
-                    notifyDataSetChanged();
-                }
-            });
-            
             announcementsRequest = new CookieRequest(Request.Method.POST, "/f/getAnnouncementsForUser", preferences, new Response.Listener<String>() {
                 @Override
                 public void onResponse(String response) {
@@ -129,15 +281,15 @@ public class HomeFragment extends Fragment {
                                     object.getJSONObject("author").getString("profpicpath")));
 
                             final int announcementNum = i;
-                            
+
                             ImageCookieRequest profPicRequest = new ImageCookieRequest("http://www.morteam.com" + announcements.get(i).picSrc + "-60",
                                     preferences,
                                     new Response.Listener<Bitmap>() {
-                                @Override
-                                public void onResponse(Bitmap response) {
-                                    announcements.get(announcementNum).setPic(response);
-                                }
-                            }, 0, 0, null, Bitmap.Config.RGB_565, new Response.ErrorListener() {
+                                        @Override
+                                        public void onResponse(Bitmap response) {
+                                            announcements.get(announcementNum).setPic(response);
+                                        }
+                                    }, 0, 0, null, Bitmap.Config.RGB_565, new Response.ErrorListener() {
                                 @Override
                                 public void onErrorResponse(VolleyError error) {
                                     System.out.println(error.networkResponse.data);
@@ -147,7 +299,7 @@ public class HomeFragment extends Fragment {
                         }
 
                         //Tell adapter to update once request is finished
-                        swipeLayout.setRefreshing(false);
+                        refreshLayout.setRefreshing(false);
                     } catch (JSONException e) {
                         e.printStackTrace();
                     }
