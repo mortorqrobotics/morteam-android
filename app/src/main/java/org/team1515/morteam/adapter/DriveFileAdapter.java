0d5eec0bce2cadc8770aa8db84999eb4cecebff2
Pdbz199 <279pdbz199@gmail.com>
2017-02-01 16:55:49 -0800

    Added drive icons

--- app/src/main/java/org/team1515/morteam/adapter/DriveFileAdapter.java
+++ app/src/main/java/org/team1515/morteam/adapter/DriveFileAdapter.java
@@ -1,5 +1,8 @@
 package org.team1515.morteam.adapter;
 
+import android.content.Context;
+import android.content.Intent;
+import android.net.Uri;
 import android.support.v7.widget.CardView;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
@@ -19,18 +22,28 @@ public class DriveFileAdapter extends RecyclerView.Adapter<DriveFileAdapter.View
         ImageView filePreview;
         TextView fileName;
 
+        private final Context context;
+
         ViewHolder(CardView layout) {
             super(layout);
+
+            context = layout.getContext();
 
             filePreview = (ImageView) layout.findViewById(R.id.file_preview);
             fileName = (TextView) layout.findViewById(R.id.file_name);
 
-            //layout.setOnClickListener(this);
+            layout.setOnClickListener(this);
         }
 
         @Override
         public void onClick(View view) {
-            //TODO: Take to website --> morteam.com/files/id/:fileID
+            int position = getAdapterPosition();
+            if (position != RecyclerView.NO_POSITION) {
+                File file = files.get(position);
+
+                Intent browserIntent = new Intent(Intent.ACTION_VIEW, Uri.parse("http://www.morteam.com/api/files/id/" + file.getId()));
+                context.startActivity(browserIntent);
+            }
         }
     }
 
@@ -55,7 +68,25 @@ public class DriveFileAdapter extends RecyclerView.Adapter<DriveFileAdapter.View
     public void onBindViewHolder(final ViewHolder holder, int position) {
         final File currentFile = files.get(position);
 
-        holder.filePreview.setImageResource(R.drawable.file);
+        if (currentFile.getType().equals("word")) {
+            holder.filePreview.setImageResource(R.drawable.doc);
+        } else if (currentFile.getType().equals("pdf")) {
+            holder.filePreview.setImageResource(R.drawable.pdf);
+        } else if (currentFile.getMimeType().substring(6).equals("pjpeg")) {
+            holder.filePreview.setImageResource(R.drawable.jpg);
+        } else if (currentFile.getMimeType().substring(6).equals("png")) {
+            holder.filePreview.setImageResource(R.drawable.png);
+        } else if (currentFile.getType().equals("audio")) {
+            holder.filePreview.setImageResource(R.drawable.mp3);
+        } else if (currentFile.getType().equals("video")) {
+            holder.filePreview.setImageResource(R.drawable.mp4);
+        } else {
+            holder.filePreview.setImageResource(R.drawable.file);
+
+            System.out.println("FILE TYPE: " + currentFile.getType());
+            System.out.println("FILE MIME TYPE: " + currentFile.getMimeType());
+        }
+
         holder.fileName.setText(currentFile.getName());
     }
 
